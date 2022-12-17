1a8a4eda7be62a8fe3486845885f2440224b1b5b
Hao Yeh <88352138+jackromeo0891111@users.noreply.github.com>
2022-12-17 15:55:56 -0800

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -19,90 +19,90 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 staging_events_table_create= ("""
 CREATE TABLE staging_events (
-event_id INT IDENTITY(0,1) PRIMARY KEY,
-artist VARCHAR,
-auth VARCHAR,
-firstName VARCHAR,
-gender VARCHAR,
-itemInSession INT,
-lastName VARCHAR,
-length DOUBLE PRECISION,
-level VARCHAR,
-location VARCHAR,
-method VARCHAR,
-page VARCHAR,
-registration DOUBLE PRECISION,
-sessionId INT,
-song VARCHAR,
-status INT,
-ts BIGINT,
-userAgent VARCHAR,
-userId VARCHAR)
+    event_id           INT     IDENTITY(0,1)     PRIMARY KEY,
+    artist             VARCHAR,
+    auth               VARCHAR,
+    first_name         VARCHAR,
+    gender             VARCHAR,
+    item_in_session    INT,
+    last_name          VARCHAR,
+    length             DOUBLE PRECISION,
+    level              VARCHAR,
+    location           VARCHAR,
+    method             VARCHAR,
+    page               VARCHAR,
+    registration       DOUBLE PRECISION,
+    session_id         INT,
+    song               VARCHAR,
+    status             INT,
+    ts                 BIGINT,
+    user_agent         VARCHAR,
+    user_id            INT)
 """)
 
 staging_songs_table_create = ("""
 CREATE TABLE staging_songs (
-artist_id VARCHAR,
-artist_latitude FLOAT,
-artist_location VARCHAR,
-artist_longitude FLOAT,
-artist_name VARCHAR,
-duration FLOAT,
-num_songs INT,
-song_id VARCHAR,
-title VARCHAR,
-year INT)
+artist_id          VARCHAR    PRIMARY KEY,
+artist_latitude    FLOAT,
+artist_location    VARCHAR,
+artist_longitude   FLOAT,
+artist_name        VARCHAR,
+duration           FLOAT,
+num_songs          INT,
+song_id            VARCHAR,
+title              VARCHAR,
+year               INT)
 """)
 
 songplay_table_create = ("""
 CREATE TABLE songplays (
-songplay_id BIGINT IDENTITY PRIMARY KEY,
-start_time BIGINT NOT NULL,
-user_id INT NOT NULL,
-level VARCHAR,
-song_id VARCHAR,
-artist_id VARCHAR,
-session_id VARCHAR,
-location VARCHAR,
-user_agent VARCHAR)
+songplay_id   BIGINT    IDENTITY    PRIMARY KEY,
+start_time    BIGINT    NOT NULL,
+user_id       INT       NOT NULL,
+level         VARCHAR,
+song_id       VARCHAR,
+artist_id     VARCHAR,
+session_id    VARCHAR,
+location      VARCHAR,
+user_agent    VARCHAR)
 """)
 
 user_table_create = ("""
 CREATE TABLE users (
-user_id INT PRIMARY KEY,
-first_name VARCHAR,
-last_name VARCHAR,
-gender VARCHAR,
-level VARCHAR)
+user_id INT   PRIMARY KEY,
+first_name    VARCHAR,
+last_name     VARCHAR,
+gender        VARCHAR,
+level         VARCHAR)
 """)
 
 song_table_create = ("""
 CREATE TABLE songs (
-song_id VARCHAR PRIMARY KEY,
-title VARCHAR NOT NULL,
-artist_id VARCHAR,
-year INT,
-duration NUMERIC NOT NULL)
+song_id    VARCHAR    PRIMARY KEY,
+title      VARCHAR    NOT NULL,
+artist_id  VARCHAR,
+year       INT,
+duration   NUMERIC NOT NULL)
 """)
 
 artist_table_create = ("""
 CREATE TABLE artists (
-artist_id VARCHAR PRIMARY KEY,
-name VARCHAR NOT NULL,
-location VARCHAR,
-latitude DOUBLE PRECISION,
-longitude DOUBLE PRECISION)
+artist_id    VARCHAR    PRIMARY KEY,
+name         VARCHAR    NOT NULL,
+location     VARCHAR,
+latitude     DOUBLE PRECISION,
+longitude    DOUBLE PRECISION)
 """)
 
 time_table_create = ("""
 CREATE TABLE time (
-start_time TIMESTAMP PRIMARY KEY,
-hour INT,
-day INT,
-week INT,
-month INT,
-year INT,
-weekday INT)
+start_time    TIMESTAMP    PRIMARY KEY,
+hour          INT,
+day           INT,
+week          INT,
+month         INT,
+year          INT,
+weekday       INT)
 """)
 
 # STAGING TABLES
@@ -127,28 +127,28 @@ json 'auto'
 
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT se.ts, se.userId, se.level, ss.song_id, ss.artist_id, se.sessionId, se.location, se.userAgent
+SELECT se.ts, se.user_id, se.level, ss.song_id, ss.artist_id, se.session_id, se.location, se.user_agent
 FROM staging_events se, staging_songs ss 
 """)
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
-SELECT se.userId, se.firstName, se.lastName, se.gender, se.level
-FROM staging_events se
+SELECT DISTINCT user_id, first_name, last_name, gender, level
+FROM staging_events
+WHERE page='NextSong'
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
-SELECT ss.song_id, ss.title, ss.artist_id, ss.year, ss.duration
-FROM staging_songs ss
+SELECT song_id, title, artist_id, year, duration
+FROM staging_songs
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
-SELECT ss.artist_id, ss.artist_name, ss.artist_location, ss.artist_latitude, ss.artist_longitude
-FROM staging_songs ss 
+SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+FROM staging_songs
 """)
-
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
@@ -161,6 +161,7 @@ EXTRACT (YEAR FROM a.start_time),
 EXTRACT (WEEKDAY FROM a.start_time)
 FROM (SELECT TIMESTAMP 'epoch' + start_time/1000 *INTERVAL '1 second' as start_time FROM songplays) a
 """)
+
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
