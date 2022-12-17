de233275dc357ccb1c3313118a57293ed7023b1a
Hao Yeh <88352138+jackromeo0891111@users.noreply.github.com>
2022-12-17 15:56:47 -0800

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,28 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """  
+    - Run SQL queries to copy data from S3 to staging tables
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """  
+    - Run SQL queries to insert into final tables from staging tables
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """  
+    - Parse config and log into Amazon Redshift and run above two functions
+    - Close connection after finished
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
